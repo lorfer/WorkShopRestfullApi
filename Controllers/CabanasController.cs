633f39fc07efd937b019e6c15095b326bb9b57a5
lorfer <fernandogarciaolivo@gmail.com>
2019-08-02 10:24:28 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/CabanasController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkShopRestfullApi.Model;
+
+namespace WorkShopRestfullApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CabanasController : ControllerBase
+    {
+        private readonly CabanasContext _context;
+
+        public CabanasController(CabanasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cabanas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cabana>>> GetCabanas()
+        {
+            return await _context.Cabanas.ToListAsync();
+        }
+
+        // GET: api/Cabanas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cabana>> GetCabana(int id)
+        {
+            var cabana = await _context.Cabanas.FindAsync(id);
+
+            if (cabana == null)
+            {
+                return NotFound();
+            }
+
+            return cabana;
+        }
+
+        // PUT: api/Cabanas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCabana(int id, Cabana cabana)
+        {
+            if (id != cabana.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cabana).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CabanaExists(id))
+                {
+                    return NotFound(); 
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cabanas
+        [HttpPost]
+        public async Task<ActionResult<Cabana>> PostCabana(Cabana cabana)
+        {
+            _context.Cabanas.Add(cabana);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCabana", new { id = cabana.Id }, cabana);
+        }
+
+        // DELETE: api/Cabanas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Cabana>> DeleteCabana(int id)
+        {
+            var cabana = await _context.Cabanas.FindAsync(id);
+            if (cabana == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cabanas.Remove(cabana);
+            await _context.SaveChangesAsync();
+
+            return cabana;
+        }
+
+        private bool CabanaExists(int id)
+        {
+            return _context.Cabanas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using WorkShopRestfullApi.Model;
+
+namespace WorkShopRestfullApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment _hostingEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("V1", new Info { Title = "CabanasRD", Version = "V1" });
+
+               
+            });
+
+            services.AddDbContext<CabanasContext>(
+
+                options =>
+                //options.UseMemoryCache(databasename: "cabanas"));
+                options.UseSqlite($"Data Source = {_hostingEnvironment.ContentRootPath}/{Configuration.GetConnectionString("DefaultConnection")}"));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+            });
+
+            app.UseMvc();
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+
+            //app.UseHttpsRedirection();
+            //app.UseMvc();
+        }
+    }
+}
